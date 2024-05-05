8b743a8d4f14e7ef9e4ba520dbb32759451dddc6
Rudra Sen <rudra_sen@outlook.com>
2024-05-05 12:23:05 +0530

    Update

---
+++ WeatherBox/README.md
@@ -0,0 +1,22 @@
+# WeatherBox
+
+## About
+
+**Simple Weather App By Using OpenWeatherMap API & Custom Icons.**
+
+## Screenshot 🖼️
+
+<img src="https://api.microlink.io?url=https%3A%2F%2Fweatherbox.complabs.in&overlay.browser=dark&screenshot=true&meta=false&embed=screenshot.url"></img>
+
+<br>
+</br>
+<br>
+</br>
+<hr>
+<footer>
+  <p align="center" style="font-size: smaller;">
+    <b>
+    Copyright © WeatherBox. Made with <span>❤</span> by <a href="https://complabs.in" target="_blank">The Comp Labs Team</a>.
+    </b>
+  </p>
+</footer>
---
+++ WeatherBox/src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>WeatherBox | Tech Fiddle</title>
+        <link rel="icon" href="/favicon.ico" sizes="any"></link>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <meta name="ahrefs-site-verification" content="7ab0a68e05075caacea14ec582e20addd6bc7aee74920f605dcb35fed3660ef2"></meta>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
